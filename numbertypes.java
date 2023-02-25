5e62f08aa91d28da56dcf5f5c6fc1646a3dd8833
Kesav-21 <cgkesav@gmail.com>
2023-02-25 18:28:03 +0530

    Number types

---
+++ numbertypes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class numbertypes{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to be checked whether postive or negative");
+        int n=sc.nextInt();
+        if(n<0){
+            System.out.println("The Entered Number is Negative");
+        }
+        else if(n>0){
+            System.out.println("The Entered Number is Postive");
+        }
+        else{
+            System.out.println("The Entered Number is Zero");
+        }
+    }
+}
