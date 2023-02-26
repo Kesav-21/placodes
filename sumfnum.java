0b7f830586821e3c251c65d7d867b769a500afa3
Kesav-21 <cgkesav@gmail.com>
2023-02-26 18:33:27 +0530

    Given sum whole Numbers

--- sumfnum.java
+++ sumfnum.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-
-public class sumnum {
+public class sumfnum {
+   
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Number to be find sum:");
         int n=sc.nextInt();
         int sum=0;
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<=n;i++){
             sum=sum+i;
         }
         System.out.println("The sum of the first n natural numbers:"+sum); 
         sc.close();      
     }
 }
+
