dceddcb89b30be3471b7166b6e46147bb555a62c
Kesav-21 <cgkesav@gmail.com>
2023-02-26 18:44:43 +0530

    Gretest two numbers

---
+++ greatesttwo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class greatesttwo {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Number 1: ");
+        int n1=sc.nextInt();
+        System.out.print("Number 2: ");
+        int n2=sc.nextInt();
+        if(n1<n2)
+        System.out.println("Number 2 is greater than Number 1");
+        else
+        System.out.println("Number 1 is greater than Number 2");        
+    }    
+}
