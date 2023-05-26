82622c580e8e4aa883d72fb1648c66b006aef875
Kesav-21 <cgkesav@gmail.com>
2023-05-26 23:08:54 +0530

    primenumber problem

---
+++ prime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class prime{
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        boolean flag=true;
+        if(n<2){
+            flag=false;
+        }
+        else{
+            for(int i=2;i<n;i++){
+                if(n%i==0){
+                    flag=false;
+                    break;
+                }
+            }
+        }
+        String result = flag ? "Prime" : "not Prime";
+     System.out.println ("The number " + n + " is : " + result);
+    }
+}
